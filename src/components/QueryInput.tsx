b05efc00ff6ae50e98da2d8175b5af2c908f2509
mizdra <pp.mizdra@gmail.com>
2021-04-24 17:40:20 +0900

    リファクタリング

---
+++ src/components/QueryInput.tsx
@@ -0,0 +1,47 @@
+import { FunctionComponent, JSX } from 'preact';
+import { useCallback, useEffect, useRef } from 'preact/hooks';
+import { JSXInternal } from 'preact/src/jsx';
+import { getCursor } from '../lib/scrapbox';
+
+export type QueryInputProps = {
+  defaultQuery: string;
+  onInput: (newQuery: string) => void;
+  onBlur: () => void;
+};
+
+function useStyles() {
+  const cursor = getCursor();
+  const queryInputStyle: JSXInternal.CSSProperties = {
+    position: 'absolute',
+    top: cursor.top,
+    left: cursor.left,
+  };
+  return { queryInputStyle };
+}
+
+export const QueryInput: FunctionComponent<QueryInputProps> = ({ defaultQuery, onInput, onBlur }) => {
+  const ref = useRef<HTMLInputElement>();
+  const { queryInputStyle } = useStyles();
+
+  useEffect(() => {
+    ref.current.focus();
+  });
+  const handleInput = useCallback(
+    (e: JSX.TargetedEvent<HTMLInputElement, Event>) => {
+      if (e.currentTarget?.value) onInput(e.currentTarget.value);
+    },
+    [onInput],
+  );
+
+  return (
+    <input
+      ref={ref}
+      className="form-control"
+      style={queryInputStyle}
+      value={defaultQuery}
+      default
+      onInput={handleInput}
+      onBlur={onBlur}
+    />
+  );
+};
--- src/components/SuggestionBox.tsx
+++ src/components/SuggestionBox.tsx
@@ -1,46 +1,20 @@
-import { FunctionComponent, JSX } from 'preact';
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks';
-import { JSXInternal } from 'preact/src/jsx';
-import { getCursor } from '../lib/scrapbox';
+import { FunctionComponent } from 'preact';
+import { useState } from 'preact/hooks';
 import { PopupMenu } from './PopupMenu';
+import { QueryInput } from './QueryInput';
 
 type SuggestionBoxProps = {
   onSelect: (iconPath: string) => void;
   onClose: () => void;
 };
 
-function useStyles() {
-  const cursor = getCursor();
-  const queryInputStyle: JSXInternal.CSSProperties = {
-    position: 'absolute',
-    top: cursor.top,
-    left: cursor.left,
-  };
-  return { queryInputStyle };
-}
-
 export const SuggestionBox: FunctionComponent<SuggestionBoxProps> = ({ onSelect, onClose }) => {
-  const queryInputRef = useRef<HTMLInputElement>();
   const [query, setQuery] = useState('');
-  const { queryInputStyle } = useStyles();
-
-  const handleInput = useCallback((e: JSX.TargetedEvent<HTMLInputElement, Event>) => {
-    if (e.currentTarget?.value) setQuery(e.currentTarget.value);
-  }, []);
-  useEffect(() => {
-    queryInputRef.current.focus();
-  });
 
   return (
     <div>
-      <PopupMenu query={query} onSelect={onSelect} onClose={onClose} />;
-      <input
-        ref={queryInputRef}
-        className="form-control"
-        style={queryInputStyle}
-        onInput={handleInput}
-        onBlur={onClose}
-      />
+      <PopupMenu query={query} onSelect={onSelect} onClose={onClose} />
+      <QueryInput defaultQuery={query} onInput={setQuery} onBlur={onClose} />
     </div>
   );
 };
