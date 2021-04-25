efd29efc0e42a446c817eda2fa1d1e5f5c38009a
mizdra <pp.mizdra@gmail.com>
2021-04-25 13:10:34 +0900

    fix: フォーカスのある行のアイコンが suggest されない問題を修正

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,27 @@ import { uniqBy } from './lib/collection';
 import { calcCursorPosition, scanIconsFromEditor } from './lib/scrapbox';
 import { CursorPosition, Icon } from './types';
 
+const cursor = document.querySelector<HTMLElement>('.cursor')!;
+
+function generateItems(projectName: string, editor: HTMLElement) {
+  const icons = scanIconsFromEditor(projectName, editor);
+  return uniqBy(icons, (icon) => icon.pagePath).map((icon) => ({
+    element: (
+      <span>
+        <img
+          alt={icon.imgAlt}
+          title={icon.imgTitle}
+          style="width: 1.3em; height: 1.3em; object-fit: contain;"
+          src={icon.imgSrc}
+        />
+        {' ' + icon.pagePath}
+      </span>
+    ),
+    searchableText: icon.pagePath,
+    value: icon,
+  }));
+}
+
 type AppProps = {
   isSuggestionOpenKeyDown: (e: KeyboardEvent) => boolean;
   editor: HTMLElement;
@@ -34,39 +55,17 @@ export const App: FunctionComponent<AppProps> = ({ isSuggestionOpenKeyDown, edit
 
   const handleKeydown = useCallback(
     (e: KeyboardEvent) => {
-      const isSuggestionOpen = isSuggestionOpenKeyDown(e);
-      if (!open && isSuggestionOpen) {
+      if (!open && isSuggestionOpenKeyDown(e)) {
         e.preventDefault();
         e.stopPropagation();
-        const cursorPosition = calcCursorPosition(window, document.querySelector<HTMLElement>('.cursor')!);
-        setCursorPosition(cursorPosition);
-
-        const projectName = scrapbox.Project.name;
-        const icons = scanIconsFromEditor(projectName, editor);
-        const newItems = uniqBy(icons, (icon) => icon.pagePath).map((icon) => ({
-          element: (
-            <span>
-              <img
-                alt={icon.imgAlt}
-                title={icon.imgTitle}
-                style="width: 1.3em; height: 1.3em; object-fit: contain;"
-                src={icon.imgSrc}
-              />
-              {' ' + icon.pagePath}
-            </span>
-          ),
-          searchableText: icon.pagePath,
-          value: icon,
-        }));
-        setItems(newItems);
+        setCursorPosition(calcCursorPosition(window, cursor));
 
         // NOTE: ある行にフォーカスがあると、行全体がテキスト化されてしまい、`scanIconsFromEditor` で
         // アイコンを取得することができなくなってしまう。そのため、予めフォーカスを外し、フォーカスのあった
         // 行のアイコン記法が画像化されるようにしておく。
         textInput.blur();
-        requestAnimationFrame(() => {
-          setOpen(true);
-        });
+        setItems(generateItems(scrapbox.Project.name, editor));
+        setOpen(true);
       }
     },
     [editor, isSuggestionOpenKeyDown, open, textInput],
