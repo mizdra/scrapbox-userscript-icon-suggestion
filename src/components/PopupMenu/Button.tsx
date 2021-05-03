2bfcbe697fb2f6b03e7f2973e1e4cecc6d5e011c
mizdra <pp.mizdra@gmail.com>
2021-05-04 00:43:01 +0900

    refactor: Button コンポーネントのテストがしやすいようにリファクタリング

--- src/components/PopupMenu/Button.tsx
+++ src/components/PopupMenu/Button.tsx
@@ -1,11 +1,10 @@
 import { VNode } from 'preact';
-import { Item } from '../PopupMenu';
 
-export type PopupMenuButtonProps<T extends VNode, U> = {
+export type PopupMenuButtonProps = {
   selected?: boolean;
-  item: Item<T, U>;
+  children: VNode;
 };
 
-export function PopupMenuButton<T extends VNode, U>({ item, selected }: PopupMenuButtonProps<T, U>) {
-  return <div className={selected ? 'button selected' : 'button'}>{item.element}</div>;
+export function PopupMenuButton<T extends VNode, U>({ children, selected }: PopupMenuButtonProps<T, U>) {
+  return <div className={selected ? 'button selected' : 'button'}>{children}</div>;
 }
