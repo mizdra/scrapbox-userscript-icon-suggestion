968977311c6d5c3ccaede4fba5307baa4fe0eecd
mizdra <pp.mizdra@gmail.com>
2021-05-04 02:42:18 +0900

    test: faker 導入

--- test/setup/jest.setup.js
+++ test/setup/jest.setup.js
@@ -1,2 +1,15 @@
-// eslint-disable-next-line no-undef
+/* eslint-env node */
+
 require('@testing-library/jest-dom');
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const faker = require('faker');
+faker.locale = 'ja';
+const seed = process.env.FAKER_SEED ?? getRandomInt(0, 0xffffff);
+faker.seed(seed);
+console.log(`faker's seed is \`${seed}\``);
