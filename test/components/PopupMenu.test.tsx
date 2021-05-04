ad9c1537635a7feadd55a634c25fa9bf58b53f39
mizdra <pp.mizdra@gmail.com>
2021-05-04 19:14:24 +0900

    test: アイテムが1つも無い時のテストを追加

--- test/components/PopupMenu.test.tsx
+++ test/components/PopupMenu.test.tsx
@@ -1,4 +1,6 @@
 import { act, fireEvent, render } from '@testing-library/preact';
+import { datatype } from 'faker';
+import { ComponentChild } from 'preact';
 import { PopupMenu } from '../../src/components/PopupMenu';
 import { CursorPosition } from '../../src/types';
 import { createEditor } from '../helpers/html';
@@ -10,6 +12,7 @@ const items = [<span key="1">item1</span>, <span key="2">item2</span>, <span key
 
 // テストに利用するイベント
 const keydownEnterEvent = new KeyboardEvent('keydown', { key: 'Enter' });
+const keydownEscapeEvent = new KeyboardEvent('keydown', { key: 'Escape' });
 
 // <QueryInput> は .editor 要素が document にあることを前提にしているので、 document に .editor を埋め込んでおく
 const editor = createEditor();
@@ -21,9 +24,10 @@ describe('PopupMenu', () => {
       const { queryByTestId } = render(<PopupMenu open={false} cursorPosition={cursorPosition} items={items} />);
       expect(queryByTestId('popup-menu')).toBeNull();
     });
-    test('Enter を押下しても、onSelect も onSelectNonexistent も呼び出されない', async () => {
+    test('Enter や Escape を押下しても、onSelect / onSelectNonexistent / onClose は呼び出されない', async () => {
       const onSelect = jest.fn();
       const onSelectNonexistent = jest.fn();
+      const onClose = jest.fn();
       render(
         <PopupMenu
           open={false}
@@ -31,14 +35,17 @@ describe('PopupMenu', () => {
           items={items}
           onSelect={onSelect}
           onSelectNonexistent={onSelectNonexistent}
+          onClose={onClose}
         />,
       );
 
       await act(() => {
         fireEvent(document, keydownEnterEvent);
+        fireEvent(document, keydownEscapeEvent);
       });
       expect(onSelect).toBeCalledTimes(0);
       expect(onSelectNonexistent).toBeCalledTimes(0);
+      expect(onClose).toBeCalledTimes(0);
     });
   });
   describe('ポップアップが開いている時', () => {
@@ -47,12 +54,36 @@ describe('PopupMenu', () => {
       expect(queryByTestId('popup-menu')).not.toBeNull();
     });
     describe('アイテムが1つも無い時', () => {
-      test.todo('空であることを表すメッセージが表示される');
-      test.todo('Enter 押下で onSelectNonexistent が呼び出される');
-      test.todo('Escape 押下で onClose が呼び出される');
-      test.todo(
-        'Tab / Shift+Tab / Enter / Escape が押下されても、選択中のアイテムが変わったり、onSelect や onClose が呼び出されることがない',
-      );
+      const items: ComponentChild[] = [];
+      test('空であることを表すメッセージが表示される', () => {
+        const emptyMessage = datatype.string();
+        const { getByText } = render(
+          <PopupMenu open cursorPosition={cursorPosition} items={items} emptyMessage={emptyMessage} />,
+        );
+        expect(getByText(emptyMessage)).toBeVisible();
+      });
+      test('Enter 押下で onSelectNonexistent が呼び出される', async () => {
+        const onSelectNonexistent = jest.fn();
+        render(
+          <PopupMenu open cursorPosition={cursorPosition} items={items} onSelectNonexistent={onSelectNonexistent} />,
+        );
+
+        expect(onSelectNonexistent).toBeCalledTimes(0);
+        await act(() => {
+          fireEvent(document, keydownEnterEvent);
+        });
+        expect(onSelectNonexistent).toBeCalledTimes(1);
+      });
+      test('Escape 押下で onClose が呼び出される', async () => {
+        const onClose = jest.fn();
+        render(<PopupMenu open cursorPosition={cursorPosition} items={items} onClose={onClose} />);
+
+        expect(onClose).toBeCalledTimes(0);
+        await act(() => {
+          fireEvent(document, keydownEscapeEvent);
+        });
+        expect(onClose).toBeCalledTimes(1);
+      });
     });
     describe('アイテムが1つ以上ある時', () => {
       test.todo('Tab 押下で次のアイテムを選択できる');
