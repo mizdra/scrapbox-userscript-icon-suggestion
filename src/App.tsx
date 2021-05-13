f0805e3408eb3be63c4d5dfa59752711d82750e9
mizdra <pp.mizdra@gmail.com>
2021-05-14 00:30:36 +0900

    refactor: テストがしやすいようにリファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'preact';
-import { useCallback, useState } from 'preact/hooks';
+import { useCallback, useMemo, useState } from 'preact/hooks';
 import { SuggestionBox, Item } from './components/SuggestionBox';
 import { useDocumentEventListener } from './hooks/useDocumentEventListener';
 import { uniqBy } from './lib/collection';
@@ -36,8 +36,12 @@ type AppProps = {
 export const App: FunctionComponent<AppProps> = ({ isSuggestionOpenKeyDown, presetIcons, editor, textInput }) => {
   const [open, setOpen] = useState(false);
   const [cursorPosition, setCursorPosition] = useState<CursorPosition>({ styleTop: 0, styleLeft: 0 });
-  const [items, setItems] = useState<Item<Icon>[]>([]);
+  const [iconsInEditor, setIconsInEditor] = useState<Icon[]>([]);
   const [presetAppended, setPresetAppended] = useState(false);
+  const items = useMemo(() => {
+    if (presetAppended) return generateItems([...iconsInEditor, ...presetIcons]);
+    else return generateItems(iconsInEditor);
+  }, [iconsInEditor, presetAppended, presetIcons]);
 
   const handleSelect = useCallback(
     (item: Item<Icon>) => {
@@ -69,7 +73,6 @@ export const App: FunctionComponent<AppProps> = ({ isSuggestionOpenKeyDown, pres
       const icons = scanIconsFromEditor(scrapbox.Project.name, editor);
 
       if (open && !presetAppended) {
-        setItems([...items, ...generateItems(presetIcons)]);
         setPresetAppended(true);
       } else {
         setCursorPosition(calcCursorPosition(cursor));
@@ -78,12 +81,12 @@ export const App: FunctionComponent<AppProps> = ({ isSuggestionOpenKeyDown, pres
         // アイコンを取得することができなくなってしまう。そのため、予めフォーカスを外し、フォーカスのあった
         // 行のアイコン記法が画像化されるようにしておく。
         textInput.blur();
-        setItems(generateItems(icons));
+        setIconsInEditor(icons);
         setOpen(true);
         setPresetAppended(false);
       }
     },
-    [editor, isSuggestionOpenKeyDown, items, open, presetAppended, presetIcons, textInput],
+    [editor, isSuggestionOpenKeyDown, open, presetAppended, textInput],
   );
   useDocumentEventListener('keydown', handleKeydown, { capture: true });
 
