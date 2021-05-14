a03df63e3fb510210017b1ea0bba33093e0f88e5
mizdra <pp.mizdra@gmail.com>
2021-05-15 01:00:19 +0900

    refactor: jest.setup.js を TS 化

--- test/setup/jest.setup.ts
+++ test/setup/jest.setup.ts
@@ -1,15 +1,15 @@
 /* eslint-env node */
 
-require('@testing-library/jest-dom');
+import '@testing-library/jest-dom';
+import faker from 'faker';
 
-function getRandomInt(min, max) {
+function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-const faker = require('faker');
 faker.locale = 'ja';
-const seed = process.env.FAKER_SEED ?? getRandomInt(0, 0xffffff);
+const seed = process.env.FAKER_SEED ? +process.env.FAKER_SEED : getRandomInt(0, 0xffffff);
 faker.seed(seed);
 console.log(`faker's seed is \`${seed}\``);
