f6b8f6db15add1b4189cd5367be21c4982817353
mizdra <pp.mizdra@gmail.com>
2021-05-15 16:37:45 +0900

    test: アイコンが挿入されることをテスト

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,8 +1,17 @@
 import { act, fireEvent, render } from '@testing-library/preact';
-import { App } from '../src/App';
+import userEvent from '@testing-library/user-event';
+import { App, AppProps } from '../src/App';
+import { insertText } from '../src/lib/scrapbox';
 import { Icon } from './../src/types';
 import { createCursor, createEditor, createScrapboxAPI, createTextInput } from './helpers/html';
-import { keydownAEvent, keydownEscapeEvent } from './helpers/key';
+import { keydownAEvent, keydownCtrlLEvent, keydownEnterEvent, keydownEscapeEvent } from './helpers/key';
+
+jest.mock('../src/lib/scrapbox', () => {
+  return {
+    ...jest.requireActual('../src/lib/scrapbox'),
+    insertText: jest.fn(),
+  };
+});
 
 // ダミーの props
 const presetIcons: Icon[] = [];
@@ -11,6 +20,11 @@ const textInput = createTextInput();
 const cursor = createCursor({ styleLeft: 0, styleTop: 0 });
 const scrapbox = createScrapboxAPI();
 const props = { presetIcons, editor, textInput, cursor, scrapbox };
+
+beforeEach(() => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (insertText as any).mockReset();
+});
 
 describe('App', () => {
   describe('初期状態', () => {
@@ -39,9 +53,32 @@ describe('App', () => {
     });
   });
   describe('SuggestBox が表示されている時', () => {
-    test.todo('Escape 押下で SuggestBox が閉じる');
+    async function renderApp(props: AppProps) {
+      const renderResult = render(<App {...props} />);
+      await act(() => {
+        fireEvent(document, keydownCtrlLEvent);
+      });
+      return renderResult;
+    }
+    test('Escape 押下で SuggestBox が閉じる', async () => {
+      const { queryByTestId } = await renderApp({ ...props });
+      expect(queryByTestId('popup-menu')).toBeInTheDocument();
+      await act(() => {
+        fireEvent(document, keydownEscapeEvent);
+      });
+      expect(queryByTestId('popup-menu')).not.toBeInTheDocument();
+    });
     describe('Enter を押下した時', () => {
-      test.todo('アイテムが1つもなければ QueryInput に入力した pagePath のアイコンが挿入される');
+      test('アイテムが1つもなければ QueryInput に入力した pagePath のアイコンが挿入される', async () => {
+        const { getByTestId } = await renderApp({ ...props, editor: createEditor(), presetIcons: [] });
+        const queryInput = getByTestId('query-input');
+        userEvent.type(queryInput, 'foo');
+        expect(queryInput).toHaveValue('foo');
+        await act(() => {
+          fireEvent(document, keydownEnterEvent);
+        });
+        expect(insertText).toBeCalledWith(expect.anything(), '[foo.icon]');
+      });
       test.todo('アイテムがあれば QueryInput に入力した pagePath のアイコンが挿入される');
     });
     test.todo('isSuggestionOpenKeyDown が真になるようなキーを押下したら、presetIcons が suggest される');
