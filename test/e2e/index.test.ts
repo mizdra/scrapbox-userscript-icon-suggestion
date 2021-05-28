6fa74e70954b1c5785db5c938ba81e07905317d0
mizdra <pp.mizdra@gmail.com>
2021-05-28 20:16:15 +0900

    test: テストを仕様変更に追従させる
    - エディタにフォーカスしなくてもポップアップを出せるようになったので、テスト側も修正する

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -30,17 +30,10 @@ async function goto(url: string) {
   await page.addScriptTag({ path: resolve(__dirname, '../../dist/e2e.js'), type: 'module' });
 }
 
-test('エディタにフォーカスされている状態で Ctrl+L を押下すると、icon-suggestion が開く', async () => {
+test('エディタにページで Ctrl+L を押下すると、icon-suggestion が開く', async () => {
   await goto('https://scrapbox.io/mizdra/icon-suggestion');
 
   expect(await page.isVisible('.popup-menu')).toBeFalsy();
-
-  // Tab キーでエディタに移動してエディタにカーソルが置かれた状況を再現する。
-  // エディタ上をクリックだと何故かカーソルが表示されなかったので、こうしたハックを利用している…
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
 
   // Ctrl + L 押下
   await page.keyboard.down('Control');
@@ -48,19 +41,6 @@ test('エディタにフォーカスされている状態で Ctrl+L を押下す
   await page.keyboard.up('Control');
 
   expect(await page.isVisible('.popup-menu')).toBeTruthy();
-});
-
-test('エディタにフォーカスされていなければ icon-suggestion は開かない', async () => {
-  await goto('https://scrapbox.io/mizdra/icon-suggestion');
-
-  expect(await page.isVisible('.popup-menu')).toBeFalsy();
-
-  // Ctrl + L 押下
-  await page.keyboard.down('Control');
-  await page.keyboard.press('l');
-  await page.keyboard.up('Control');
-
-  expect(await page.isVisible('.popup-menu')).toBeFalsy();
 });
 
 test('プロジェクト一覧ページでは icon-suggestion は開かない', async () => {
