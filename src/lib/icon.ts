518df19075b8f8e7da34f4c01f720d84d702bd6f
mizdra <pp.mizdra@gmail.com>
2021-05-29 21:08:40 +0900

    feat: IconClass を実装

--- src/lib/icon.ts
+++ src/lib/icon.ts
@@ -1,5 +1,55 @@
 import { Icon } from '../types';
 
+/** アイコンを表すクラス */
+export class Icon {
+  /** アイコンが表すページが所属するプロジェクト名 */
+  readonly projectName: string;
+  /** アイコンが表すページのタイトル */
+  readonly pageTitle: string;
+  /**
+   * `Icon` クラスのコンストラクタ
+   * @param projectName アイコンが表すページが所属するプロジェクト名
+   * @param pageTitle アイコンが表すページのタイトル
+   */
+  constructor(projectName: string, pageTitle: string) {
+    this.projectName = projectName;
+    this.pageTitle = pageTitle;
+  }
+  /**
+   * アイコンが表すページのパスを返す。
+   * `currentProjectName` に所属するアイコンの場合は 相対パス形式で、
+   * そうでなければ絶対パス形式で返される。
+   * */
+  getShortPagePath(currentProjectName: string): string {
+    if (this.projectName === currentProjectName) return this.pageTitle;
+    return `/${this.projectName}/${this.pageTitle}`;
+  }
+  /** アイコンが表すページのパスを絶対パス形式で返す。 */
+  get fullPagePath(): string {
+    return `/${this.projectName}/${this.pageTitle}`;
+  }
+  /** アイコンを img タグにした時に alt 属性にセットされる値 */
+  get imgAlt(): string {
+    return this.pageTitle;
+  }
+  /** アイコンを img タグにした時に title 属性にセットされる値 */
+  get imgTitle(): string {
+    return this.pageTitle;
+  }
+  /** アイコンを img タグにした時に src 属性にセットされる値 */
+  get imgSrc(): string {
+    return `/api/pages/${this.projectName}/${encodeURIComponent(this.pageTitle)}/icon`;
+  }
+  /** アイコンが管理されているプロジェクト名 */
+  getNotation(currentProjectName: string): string {
+    return `[${this.getShortPagePath(currentProjectName)}.icon]`;
+  }
+  /** インスタンス同士が表すアイコンが一致するかどうか */
+  equals(target: Icon): boolean {
+    return this.fullPagePath === target.fullPagePath;
+  }
+}
+
 export function pagePathToIcon(currentProjectName: string, pagePath: string): Icon {
   const isRoot = pagePath.startsWith(`/`);
   const projectName = isRoot ? pagePath.slice(1, pagePath.indexOf('/', 1)) : currentProjectName;
