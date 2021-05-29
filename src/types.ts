6dcce3681b3a67695860aa00da38c495693b857a
mizdra <pp.mizdra@gmail.com>
2021-05-30 00:36:33 +0900

    feat: Promise 型もサポートする

--- src/types.ts
+++ src/types.ts
@@ -10,22 +10,11 @@ export type CursorPosition = {
 };
 
 /**
- * `presetIcons` オプションの要素の型。
- * 要素の型には以下が利用できる。
- * - `Icon`
- *   - 例: `new Icon('project', 'page')`
- * - `Icon[]`
- *   - 例: `[new Icon('project', 'page1'), new Icon('project', 'page2')]`
- * - `() => PresetIconsItem[]`
- *   - 動的に `PresetIconsItem[]` を生成して返す関数
- * - `() => Promise<PresetIconsItem[]>`
- *   - `() => PresetIconsItem[]` の非同期版
+ * `presetIcons` オプションの要素の型
  * */
 export type PresetIconsItem =
-  // TODO: 十分時間が経過したら string / string[] 型自体を PresetIconsItem に含めないようにする
-  | string // @deprecated
-  | string[] // @deprecated
   | Icon
   | Icon[]
+  | Promise<Icon | Icon[]>
   | (() => PresetIconsItem[])
   | (() => Promise<PresetIconsItem[]>);
