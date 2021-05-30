355e1b72785cc3829f3e16cfa06a3f92e4cd39bf
mizdra <pp.mizdra@gmail.com>
2021-05-30 12:50:13 +0900

    test: テストのプロトタイプを実装

---
+++ test/lib/preset-icon.test.ts
@@ -0,0 +1,10 @@
+describe('getMemberIcons', () => {
+  describe('プロジェクトが private', () => {
+    test.todo('そのプロジェクトのメンバーでない時');
+    test.todo('そのプロジェクトのメンバーである時');
+  });
+  describe('プロジェクトが public', () => {
+    test.todo('そのプロジェクトのメンバーでない時');
+    test.todo('そのプロジェクトのメンバーである時');
+  });
+});
