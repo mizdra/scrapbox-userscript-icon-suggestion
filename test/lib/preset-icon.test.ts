660d33b18bebc3fed7b9e55619f7afc2043750f1
mizdra <pp.mizdra@gmail.com>
2021-05-30 16:03:28 +0900

    feat: fetchRelatedPageIconsByHashTag を実装

--- test/lib/preset-icon.test.ts
+++ test/lib/preset-icon.test.ts
@@ -1,4 +1,5 @@
 import { getMemberIcons, Icon } from '../../src';
+import { fetchRelatedPageIconsByHashTag } from '../../src/lib/preset-icon';
 import {
   DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_GUEST,
   DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_GUEST,
@@ -12,6 +13,8 @@ import {
   DUMMY_PROJECT_JSON_FOR_PRIVATE_AND_MEMBER,
   DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_MEMBER,
   DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_MEMBER,
+  DUMMY_NON_EXIST_PAGE_JSON,
+  NON_EXIST_PAGE_JSON_URL_RE,
 } from '../fixtures/scrapbox-api';
 
 describe('getMemberIcons', () => {
@@ -47,6 +50,39 @@ describe('getMemberIcons', () => {
         .doMockOnceIf(PROJECT_JSON_URL_RE, DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_MEMBER)
         .doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_MEMBER);
       expect(await getMemberIcons('project')).toStrictEqual([new Icon('project', 'mizdra')]);
+    });
+  });
+});
+
+describe('fetchRelatedPageIconsByHashTag', () => {
+  test('プロジェクトが存在しない時', async () => {
+    fetchMock.doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_NOT_EXISTS);
+    expect(await fetchRelatedPageIconsByHashTag('project', 'member')).toStrictEqual([]);
+  });
+  describe('プロジェクトが存在する時', () => {
+    test('プロジェクトが private && そのプロジェクトのメンバーでない時', async () => {
+      fetchMock.doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PRIVATE_AND_GUEST);
+      expect(await fetchRelatedPageIconsByHashTag('project', 'member')).toStrictEqual([]);
+    });
+    test('プロジェクトが public && そのプロジェクトのメンバーでない時', async () => {
+      fetchMock.doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_GUEST);
+      expect(await fetchRelatedPageIconsByHashTag('project', 'member')).toStrictEqual([
+        new Icon('project', 'mizdra'),
+        new Icon('project', '横から失礼する人'),
+      ]);
+    });
+    describe('そのプロジェクトのメンバーである時', () => {
+      test('ハッシュタグがない時', async () => {
+        fetchMock.doMockOnceIf(NON_EXIST_PAGE_JSON_URL_RE, DUMMY_NON_EXIST_PAGE_JSON);
+        expect(await fetchRelatedPageIconsByHashTag('project', 'non-exist')).toStrictEqual([]);
+      });
+      test('ハッシュタグがある時', async () => {
+        fetchMock.doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_MEMBER);
+        expect(await fetchRelatedPageIconsByHashTag('project', 'member')).toStrictEqual([
+          new Icon('project', 'mizdra'),
+          new Icon('project', '横から失礼する人'),
+        ]);
+      });
     });
   });
 });
