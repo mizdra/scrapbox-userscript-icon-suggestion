ee6b1fb13a674468f81570cc990324d88314591c
mizdra <pp.mizdra@gmail.com>
2021-05-30 15:11:24 +0900

    test: 正常系のテストケースを追加

--- test/lib/preset-icon.test.ts
+++ test/lib/preset-icon.test.ts
@@ -1,4 +1,4 @@
-import { getMemberIcons } from '../../src';
+import { getMemberIcons, Icon } from '../../src';
 import {
   DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_GUEST,
   DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_GUEST,
@@ -8,6 +8,10 @@ import {
   DUMMY_PROJECT_JSON_FOR_NOT_EXISTS,
   DUMMY_MEMBER_PAGE_JSON_FOR_PRIVATE_AND_GUEST,
   DUMMY_PROJECT_JSON_FOR_PRIVATE_AND_GUEST,
+  DUMMY_MEMBER_PAGE_JSON_FOR_PRIVATE_AND_MEMBER,
+  DUMMY_PROJECT_JSON_FOR_PRIVATE_AND_MEMBER,
+  DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_MEMBER,
+  DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_MEMBER,
 } from '../fixtures/scrapbox-api';
 
 describe('getMemberIcons', () => {
@@ -24,7 +28,12 @@ describe('getMemberIcons', () => {
         .doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PRIVATE_AND_GUEST);
       expect(await getMemberIcons('project')).toStrictEqual([]);
     });
-    test.todo('そのプロジェクトのメンバーである時');
+    test('そのプロジェクトのメンバーである時', async () => {
+      fetchMock
+        .doMockOnceIf(PROJECT_JSON_URL_RE, DUMMY_PROJECT_JSON_FOR_PRIVATE_AND_MEMBER)
+        .doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PRIVATE_AND_MEMBER);
+      expect(await getMemberIcons('project')).toStrictEqual([new Icon('project', 'mizdra')]);
+    });
   });
   describe('プロジェクトが public', () => {
     test('そのプロジェクトのメンバーでない時', async () => {
@@ -33,6 +42,11 @@ describe('getMemberIcons', () => {
         .doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_GUEST);
       expect(await getMemberIcons('project')).toStrictEqual([]);
     });
-    test.todo('そのプロジェクトのメンバーである時');
+    test('そのプロジェクトのメンバーである時', async () => {
+      fetchMock
+        .doMockOnceIf(PROJECT_JSON_URL_RE, DUMMY_PROJECT_JSON_FOR_PUBLIC_AND_MEMBER)
+        .doMockOnceIf(MEMBER_PAGE_JSON_URL_RE, DUMMY_MEMBER_PAGE_JSON_FOR_PUBLIC_AND_MEMBER);
+      expect(await getMemberIcons('project')).toStrictEqual([new Icon('project', 'mizdra')]);
+    });
   });
 });
