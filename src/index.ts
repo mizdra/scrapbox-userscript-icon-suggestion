bc5ae6ed2152d1cab885ec4306945ee1d7cfede8
mizdra <pp.mizdra@gmail.com>
2021-09-18 17:18:42 +0900

    feat: built-in matcher を公開する

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { registerIconSuggestion } from './lib/register';
 export { Icon } from './lib/icon';
 export { fetchMemberPageIcons, fetchRelatedPageIconsByHashTag } from './lib/preset-icon';
+export { fuzzyMatcher, startsWithMatcher, includesMatcher, combinedMatcher } from './lib/matcher';
