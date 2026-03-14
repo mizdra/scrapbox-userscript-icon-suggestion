0bcac5c97afe0ded2a02b7f71886a8a83d85807b
mizdra <pp.mizdra@gmail.com>
2026-03-15 00:09:55 +0900

    scrapbox の型をいい感じに
    - REST API のものは preset-icon.ts 側に寄せておく
    - JS API のものはそのまま

--- src/lib/preset-icon.ts
+++ src/lib/preset-icon.ts
@@ -1,5 +1,29 @@
 import { Icon } from './icon';
 
+/** https://scrapbox.io/api/projects/<project-name> を叩くと返ってくる JSON の型 */
+interface ProjectJson {
+  // プロジェクトのメンバーでないなど、所属ユーザ情報にアクセスできない
+  // ユーザからのリクエスト場合はそもそもプロパティが存在しない。
+  users?: ProjectJsonUser[];
+}
+
+interface ProjectJsonUser {
+  name: string;
+}
+/** https://scrapbox.io/api/pages/<project-name>/<page-title> を叩くと返ってくる JSON の型 */
+interface PageJson {
+  // NOTE: プロジェクトのメンバーでない場合など、プロパティが存在しないことがある
+  relatedPages?: RelatedPages;
+}
+interface RelatedPages {
+  links1hop: Link[];
+}
+
+interface Link {
+  title: string;
+  image: string | null;
+}
+
 /**
  * 指定されたプロジェクトに所属するメンバーのアイコンを全て取得する。
  * ただしユーザページがないユーザや、ユーザページにアイコンが埋め込まれていないユーザ、
--- src/typings/scrapbox.d.ts
+++ src/typings/scrapbox.d.ts
@@ -1,40 +1,14 @@
-interface Project {
-  name: string;
-}
-type EventName = 'layout:changed' | 'project:changed';
 declare global {
-  interface ScrapboxAPI {
-    Layout: string;
-    Project: Project;
-    addListener: (eventName: EventName, listener: () => void) => ScrapboxAPI;
-    removeListener: (eventName: EventName, listener: () => void) => ScrapboxAPI;
-  }
-  var scrapbox: ScrapboxAPI;
-
-  /** https://scrapbox.io/api/projects/<project-name> を叩くと返ってくる JSON の型 */
-  interface ProjectJson {
-    // プロジェクトのメンバーでないなど、所属ユーザ情報にアクセスできない
-    // ユーザからのリクエスト場合はそもそもプロパティが存在しない。
-    users?: ProjectJsonUser[];
-  }
-
-  interface ProjectJsonUser {
+  interface ScrapboxProject {
     name: string;
   }
-  /** https://scrapbox.io/api/pages/<project-name>/<page-title> を叩くと返ってくる JSON の型 */
-  interface PageJson {
-    // NOTE: プロジェクトのメンバーでない場合など、プロパティが存在しないことがある
-    relatedPages?: RelatedPages;
+  type ScrapboxEventName = 'layout:changed' | 'project:changed';
+  interface ScrapboxAPI {
+    Layout: string;
+    Project: ScrapboxProject;
+    addListener: (eventName: ScrapboxEventName, listener: () => void) => ScrapboxAPI;
+    removeListener: (eventName: ScrapboxEventName, listener: () => void) => ScrapboxAPI;
   }
+  var scrapbox: ScrapboxAPI;
 }
-
-interface RelatedPages {
-  links1hop: Link[];
-}
-
-interface Link {
-  title: string;
-  image: string | null;
-}
-
 export {};
